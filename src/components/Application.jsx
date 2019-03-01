f98c8d4367ba30c263d3031c88cbe27eaf08b211
scarletks1214 <scarlet.ks1214@gmail.com>
2019-03-02 01:49:33 +0300

    fixed some warnnings

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -16,11 +16,14 @@ class Application extends Component {
         <LazyLoad height={732} offsetVertical={10}>
           <img
             src='http://apod.nasa.gov/apod/image/1502/HDR_MVMQ20Feb2015ouellet1024.jpg'
+            alt='first one'
             style={{width:'100vw'}} />
         </LazyLoad>
         <div className="filler" />
         <LazyLoad height={683} offsetVertical={10}>
-          <img src='http://apod.nasa.gov/apod/image/1502/2015_02_20_conj_bourque1024.jpg' />
+          <img
+            src='http://apod.nasa.gov/apod/image/1502/2015_02_20_conj_bourque1024.jpg'
+            alt='second one' />
         </LazyLoad>
         <div className="filler" />
         <div className="ScrollableContainer">
@@ -28,19 +31,25 @@ class Application extends Component {
           <div className="filler" />
           <div className="filler" />
           <LazyLoad height={480}>
-            <img src='http://apod.nasa.gov/apod/image/1502/MarsPlume_jaeschke_480.gif' />
+            <img
+              src='http://apod.nasa.gov/apod/image/1502/MarsPlume_jaeschke_480.gif'
+              alt='third one' />
           </LazyLoad>
         </div>
         <div className="filler" />
         <LazyLoad height={720} offsetVertical={10}>
-          <img src='http://apod.nasa.gov/apod/image/1502/ToadSky_Lane_1080_annotated.jpg' />
+          <img
+            src='http://apod.nasa.gov/apod/image/1502/ToadSky_Lane_1080_annotated.jpg'
+            alt='fourth one' />
         </LazyLoad>
         <div className="filler" />
         <LazyLoad
           height={720}
           onContentVisible={() => console.log('look ma I have been lazyloaded!')}
         >
-          <img src='http://apod.nasa.gov/apod/image/1502/ToadSky_Lane_1080_annotated.jpg' />
+          <img
+            src='http://apod.nasa.gov/apod/image/1502/ToadSky_Lane_1080_annotated.jpg'
+            alt='fifth one' />
         </LazyLoad>
         <div className="filler" />
       </div>
